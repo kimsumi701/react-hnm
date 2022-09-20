97d53ed0935ae1f65ef5c078716b3640b185ed79
Kimsumi <kimsumi701@gmail.com>
2022-09-20 18:48:49 +0900

    디테일 수정

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     let { id } = useParams();
-    const [product, setProduct] = useState('');
+    const [product, setProduct] = useState(null);
     const getProductDetail = async () => {
         let url = `https://my-json-server.typicode.com/kimsumi701/react-hnm/products/${id}`;
         let response = await fetch(url);
@@ -23,9 +23,9 @@ const ProductDetail = () => {
                 <Col>
                     <div>{product?.title}</div>
                     <div>{product?.price}</div>
-                    <div>{product?.choice == true ? 'Conscious choice' : ''}</div>
-                    <div>{product?.new == true ? '신제품' : ''}</div>
-                    <Dropdown>
+                    <div>{product?.choice ? 'Conscious choice' : ''}</div>
+                    <div>{product?.new ? '신제품' : ''}</div>
+                    <Dropdown className="drop-down">
                         <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
                             사이즈 선택
                         </Dropdown.Toggle>
